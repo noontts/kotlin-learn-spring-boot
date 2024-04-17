6c6c3079af8a84b15273bc7d2ddef497df56cc0d
thitisan.k <thitisan.k@bluebik.com>
2024-04-17 20:50:13 +0700

    feat: merchant and product mapper.

---
+++ shopoo/src/main/kotlin/com/tsdigital/shopoo/mapper/ProductMapper.kt
@@ -0,0 +1,20 @@
+package com.tsdigital.shopoo.mapper
+
+import com.tsdigital.shopoo.dto.product.ProductDTO
+import com.tsdigital.shopoo.entity.Product
+
+class ProductMapper private constructor() {
+    companion object{
+        fun toProductDto(product: Product): ProductDTO {
+            return ProductDTO(
+                uuid = product.uuid,
+                name = product.name,
+                description = product.description,
+                price = product.price,
+                inStock = product.inStock,
+                createdAt = product.createdAt,
+                updatedAt = product.updatedAt
+            )
+        }
+    }
+}
